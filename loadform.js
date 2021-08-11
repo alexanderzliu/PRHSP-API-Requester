89024be7e1d2595bb051002806679b1e6b5a9e5b
Alexander Liu <alexanderliu@berkeley.edu>
2021-08-11 06:00:31 -0700

    removed unnecessary files

--- loadform.js
+++
@@ -1,6 +0,0 @@
-async function configLoad() {
-	let environmentInput = $('#URIEnvironment');
-	let opFirst = document.createElement('option');
-	alert(environmentInput.val());
-	alert(jsonSux.Environment.length);
-	environmentInput.append(opFirst);
